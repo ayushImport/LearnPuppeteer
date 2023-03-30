c97945d88bb091667ef6398adba2c3a9ed69258e
ayushImport <ayush.m@partner.import.io>
2023-03-30 19:47:18 +0530

    puppeteer task

--- src/lyricsShot.js
+++ src/lyricsShot.js
@@ -7,32 +7,34 @@ const takeLyricsShot = async () => {
     // await page.goto('https://www.google.com');
     // await page.screenshot({ path: './LyricsShot/example2.png' });
 
-    await page.goto('https://www.azlyrics.com/', {waitUntil: 'load', timeout: 30000})
-    page.on('console', (log) => console[log._type](log._text));
+    await page.goto('https://www.azlyrics.com/', {waitUntil: 'load'})
+    // page.on('console', (log) => console[log._type](log._text));
 
     console.log('Getting all the song nodes!')
 
-    const requiredSongsList = await page.$$('a.list-group-item')
+    const requiredSongsList = await page.$$eval('a.list-group-item', el => {
+        return el.map(r => r.href)
+    })
 
-    console.log('Got required Songs!')
     let counter = 1;
-    requiredSongsList.forEach(async (el) => {
-        await new Promise (resolve => setTimeout(resolve, 5000))
-        console.log('Entered in loop')
-        const songName = await page.evaluate(async (el) => {
-            console.log('Clicking Song')
-            el.click() // Navigating to song page
-            await new Promise (resolve => setTimeout(resolve, 5000))   // Wating for page to laod
+    for (url of requiredSongsList) {
+        console.log(url)
+        console.log('Going to the Song Page!')
+
+        await page.goto(url)
+
+        const songName = await page.evaluate(async () => {
             let songName = document.querySelector('div h1') ? document.querySelector('div h1').innerText.replace(/"/g, '') : 'Song' //Getting songName
             return songName
-        }, el)
+        })
         console.log(songName)
         // await page.screenshot()
         const lyricsNode = await page.$('div.row:nth-child(2) div.text-center:nth-child(2)')
 
         await lyricsNode.screenshot({path: `./LyricsShot/Lyrics - ${songName} - ${counter}.png`})
         counter++;
-    })
+    }
+
     await page.close()
     await browser.close();
 }
