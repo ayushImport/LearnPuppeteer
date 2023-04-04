f8cc37deeec41229a14eca0f4fe68582e1034518
ayushImport <ayush.m@partner.import.io>
2023-04-04 12:29:08 +0530

    task2

---
+++ src/ecoPartner.js
@@ -0,0 +1,38 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+
+const economicalPartners = async () => {
+    const browserPath = 'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe'
+    const browser = await puppeteer.launch({executablePath: browserPath, dumpio: true})
+    const page = await browser.newPage()
+
+    await page.goto('https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)', {waitUntil: 'load'})
+
+    console.log('Getting all Wiki URLs for all the countries')
+    const requiredUrlObj = await page.evaluate(async () => {
+        const requiredUrlObj = [];
+        let countriesNode = document.querySelectorAll('table.wikitable td span.flagicon + a')
+        countriesNode.forEach((el) => {
+            requiredUrlObj.push({
+                country: el.innerText,
+                Url: el.href 
+            })
+        })
+        return requiredUrlObj;
+    })
+
+    console.log('Getting required Import/Export partner data')
+    for (obj of requiredUrlObj) {
+        console.log(obj.country)
+        await page.goto(obj.Url, { waitUntil: 'load' })
+        await page.evaluate(async () => {
+            const isInfoTableAvailable = !!document.querySelector('table.infobox')
+            console.log('Info Available', isInfoTableAvailable)
+        })
+    }
+
+    await page.close()
+    await browser.close()
+}
+
+economicalPartners()
